885f32eb3fdb183da155f3d092eb912a19ffa243
Brian Love <22353962+brianlove@users.noreply.github.com>
2024-03-23 22:59:14 -0400

    Initial commit with basic rocket controls
    Initial commit of "Project Boost", following the completion of
    lesson 15 ("The Input Map").

---
+++ Player.gd
@@ -0,0 +1,15 @@
+extends RigidBody3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("boost"):
+		#position.y += delta
+		apply_central_force(basis.y * delta * 1000)
+
+	if Input.is_action_pressed("rotate_left"):
+		#rotate_z(delta)
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+	elif Input.is_action_pressed("rotate_right"):
+		#rotate_z(-delta)
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+
